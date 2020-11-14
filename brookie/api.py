ccf9af8e3fabe50ed23b1a1bdf018edc17046c3a
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2020-11-15 00:25:41 +0100

    cache pages list on use

--- brookie/api.py
+++ brookie/api.py
@@ -9,10 +9,13 @@ from starlette.responses import StreamingResponse
 from .configuration import CONFIGURATION
 from .database import connect, disconnect, get_sync_cursor
 
+LIBRARY_PATH = CONFIGURATION.library.full_path
+
 API = FastAPI()
 EVENT = API.on_event
 GET = API.get
 POST = API.post
+WS = API.websocket
 
 EVENT("startup")(connect)
 EVENT("shutdown")(disconnect)
@@ -21,26 +24,29 @@ EVENT("shutdown")(disconnect)
 # TODO: Make async
 def get_books():
     with get_sync_cursor() as db:
-        lib = CONFIGURATION.library.full_path
+        lib = LIBRARY_PATH
         return DataFrame(
             [
-                (id, (lib / path / name).with_suffix("." + format.lower()))
-                for id, path, name, format in db.execute(
+                (id, (lib / path / name).with_suffix("." + format_.lower()), None)
+                for id, path, name, format_ in db.execute(
                     "SELECT books.id AS id, path, name, format FROM books JOIN data ON books.id = data.book"
                 )
             ],
-            columns=("id", "path"),
+            columns=("id", "path", "pages"),
         ).set_index("id")
 
 
-BOOKS_PATH: DataFrame = get_books().path
+BOOKS: DataFrame = get_books()
 
 
 @GET("/{book_id}")
 @GET("/{book_id}/{page}")
 async def _(book_id: int, page: int = 0):
-    with SeekableArchive(Path(BOOKS_PATH[book_id]).open("r")) as book:
+    book = BOOKS.loc[book_id, :]
+    with SeekableArchive(Path(book.path).open("r")) as archive:
+        pages = book.pages
+        if pages is None:
+            pages = book.pages = tuple(sorted(e.pathname for e in archive))
         return StreamingResponse(
-            BytesIO(book.read(sorted(e.pathname for e in book)[page])),
-            media_type="image/webp",
+            BytesIO(archive.read(pages[page])), media_type="image/webp",
         )
